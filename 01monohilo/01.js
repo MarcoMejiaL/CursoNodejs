d255b15abd2c391bb5784ec7620040b714e7dd4f
Marco Antonio Mejia Lopez <tomatin77@gmail.com>
2021-07-16 15:05:41 -0500

    inicio del curso

---
+++ 01monohilo/01.js
@@ -0,0 +1,7 @@
+console.log('hola mundo');
+let i =0;
+
+setInterval(function(){
+    console.log(i);
+    i++
+}, 3000)
---
+++ 02entorno/02.js
@@ -0,0 +1,2 @@
+let nombre = process.env.NOMBRE || 'taco' //se pone en mayuscula por defecto VARIABLE o MI_VARIABLE
+console.log(`hola ${nombre}`);
