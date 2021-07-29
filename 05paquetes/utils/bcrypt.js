93593a09c50ce85ee8f0bfecf1368a5ccfe97307
Marco Antonio Mejia Lopez <tomatin77@gmail.com>
2021-07-29 18:24:58 -0500

    curso terminado

--- 05paquetes/utils/bcrypt.js
+++ 05paquetes/utils/bcrypt.js
@@ -2,7 +2,13 @@ const bcrypt = require('bcrypt');
 
 const password = '12345Segura!';
 
+
 bcrypt.hash(password, 5, function(error,hash){
     console.log(hash);
+    bcrypt.compare(password,hash,function(err,result){
+        /* console.log(err); */
+        console.log(result);
+    })
     
 })
+//moment es para la hora y el sharp es para reducir el tamaña de los png
